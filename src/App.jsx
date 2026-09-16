19d9e1c4b1eaf4824ebb9662464474311484b458
Abdullah-Youssef22 <abdallah.jou3@gmail.com>
2026-09-16 14:08:34 +0300

    2.4 Host Layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { useState } from "react";
 
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+
+import Layout from "./components/Layout";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
-import Vans from "./Pages/Vans";
-import VanDetails from "./Pages/VanDetails";
-import Layout from "./components/Layout";
+import Vans from "./Pages/Vans/Vans";
+import VanDetails from "./Pages/Vans/VanDetails";
+
+import HostLayout from "./components/HostLayout";
+import Dashboard from "./Pages/Host/Dashboard";
+import Income from "./Pages/Host/Income";
+import Reviews from "./Pages/Host/Reviews";
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <BrowserRouter>
       <Routes>
@@ -19,10 +25,16 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetails />} />
+          
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
 export default App;
